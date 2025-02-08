82cc572a33926c6aa2cf2ddaf818128fd7553d7e
Wayne Luke <wayneluke@icloud.com>
2025-02-07 16:50:51 -0800

    re-add scripts

---
+++ brew.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Brew install and setup
+xcode-select --install
+
+# Check for Homebrew, install if not installed
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# Enables brew in current env
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Disables brew telemetry
+echo -e "Disabling Homebrew telemetry..."
+brew analytics off
+
+# Brew Apps installed from Brewfile
+echo -e "Installing apps..."
+brew bundle install --file Brewfile
